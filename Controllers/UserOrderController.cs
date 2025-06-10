e14de21f5a59872b0287a6bcd7417bd4bbc82342
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2025-06-11 02:47:02 +0500

    done User Order Controllers too

---
+++ Controllers/UserOrderController.cs
@@ -0,0 +1,110 @@
+﻿using EcommerceAPI.DTOs.OrderDTOs;
+using EcommerceAPI.Services.UserManagement.UserOrderManagement;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserOrderController : ControllerBase
+    {
+        private readonly IUserOrder userOrderService;
+
+        public UserOrderController(IUserOrder userOrderService)
+        {
+            this.userOrderService = userOrderService;
+        }
+
+        [HttpGet("MyOrders")]
+        public async Task<ActionResult<List<UserOrderResponseDTO>>> GetMyOrdersAsync()
+        {
+            var userIdClaim = User.FindFirst("UserId")?.Value;
+            if (!int.TryParse(userIdClaim, out int userId) || userId <= 0)
+            {
+                return Unauthorized("User is not authorized");
+            }
+
+            var response = await userOrderService.GetMyOrdersAsync(userId);
+            if (response.Success == false)
+            {
+                return NotFound(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+
+        [HttpGet("OrderDetails/{orderId}")]
+        public async Task<ActionResult<UserOrderDetailsResponseDTO>> GetOrderDetailsAsync(int orderId)
+        {
+            var userIdClaim = User.FindFirst("UserId")?.Value;
+            if (!int.TryParse(userIdClaim, out int userId) || userId <= 0)
+            {
+                return Unauthorized("User is not authorized");
+            }
+
+            var response = await userOrderService.GetOrderDetailsAsync(userId, orderId);
+            if (response.Success == false)
+            {
+                return NotFound(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+
+        [HttpPost("CheckoutPreview")]
+        public async Task<ActionResult<List<CheckoutPreviewResponseDTO>>> GetCheckoutPreviewAsync([FromBody] List<int> cartItemIds)
+        {
+            var userIdClaim = User.FindFirst("UserId")?.Value;
+            if (!int.TryParse(userIdClaim, out int userId) || userId <= 0)
+            {
+                return Unauthorized("User is not authorized");
+            }
+
+            var response = await userOrderService.GetCheckoutPreviewAsync(userId, cartItemIds);
+            if (response.Success == false)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+
+        [HttpPost("place-order")]
+        public async Task<ActionResult<int>> PlaceOrderAsync([FromBody] CreateOrderDTO request)
+        {
+            var userIdClaim = User.FindFirst("UserId")?.Value;
+            if (!int.TryParse(userIdClaim, out int userId) || userId <= 0)
+            {
+                return Unauthorized("User is not authorized");
+            }
+
+            var response = await userOrderService.PlaceOrderAsync(userId, request);
+            if (response.Success == false)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(new { OrderId = response.Data , Message = "Order Placed Succesfully"}); // Order ID
+        }
+
+        [HttpPost("cancel/{orderId}")]
+        public async Task<ActionResult<string>> CancelOrderAsync(int orderId)
+        {
+            var userIdClaim = User.FindFirst("UserId")?.Value;
+            if (!int.TryParse(userIdClaim, out int userId) || userId <= 0)
+            {
+                return Unauthorized("User is not authorized");
+            }
+
+            var response = await userOrderService.CancelOrderAsync(userId, orderId);
+            if (response.Success == false)
+            {
+                return BadRequest(response.Message);
+            }
+
+            return Ok(response.Data);
+        }
+    }
+}
--- obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91eadaf3bdb1d1fdc0e582fbf15f1019a54c77e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15141ff57614c5da67e4df9dfd82407dfdc3eb8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
