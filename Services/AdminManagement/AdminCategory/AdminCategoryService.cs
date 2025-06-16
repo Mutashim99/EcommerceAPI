a4804556875d44583af0b134ae3903dab4a1e9c9
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2025-06-16 23:30:38 +0500

    start Admin Operations : done category management service

--- Services/AdminManagement/AdminCategory/AdminCategoryService.cs
+++ Services/AdminManagement/AdminCategory/AdminCategoryService.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using EcommerceAPI.DTOs.CategoryDTOs;
+using EcommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceAPI.Services.AdminManagement.AdminCategory
 {
@@ -13,30 +15,109 @@ namespace EcommerceAPI.Services.AdminManagement.AdminCategory
             this.db = db;
             this.mapper = mapper;
         }
-        public Task<ServiceResponse<CategoryResponseDTO>> CreateCategoryAsync(CreateCategoryDTO dto)
+        public async Task<ServiceResponse<CategoryResponseDTO>> CreateCategoryAsync(CreateCategoryDTO dto)
         {
-
-            throw new NotImplementedException();
+            if (dto == null)
+            {
+                return new ServiceResponse<CategoryResponseDTO>()
+                {
+                    Message = "Data is Null",
+                    Success = false,
+                    Data = null
+                };
+            }
+            var MappedCategory = mapper.Map<Category>(dto);
+            await db.AddAsync(MappedCategory);
+            await db.SaveChangesAsync();
+            var mappedIntoResponse = mapper.Map<CategoryResponseDTO>(MappedCategory);
+            return new ServiceResponse<CategoryResponseDTO>()
+            {
+                Message = "Category added Succesfully",
+                Success = true,
+                Data = mappedIntoResponse
+            };
         }
 
-        public Task<ServiceResponse<string>> DeleteCategoryAsync(int id)
+        public async Task<ServiceResponse<string>> DeleteCategoryAsync(int categoryId)
         {
-            throw new NotImplementedException();
+            var categoryFromDb = await db.Categories.FirstOrDefaultAsync(x=> x.Id == categoryId);
+            if (categoryFromDb == null)
+            {
+                return new ServiceResponse<string>()
+                {
+                    Message = "cant find the category with given Id",
+                    Success = false,
+                    Data = null
+                };
+            }
+            db.Remove(categoryFromDb);
+           await db.SaveChangesAsync();
+            return new ServiceResponse<string>()
+            {
+                Message = "Category deleted succesfully",
+                Success = true,
+                Data = "Category deleted succesfully"
+            };
         }
 
-        public Task<ServiceResponse<List<CategoryResponseDTO>>> GetAllCategoriesAsync()
+        public async Task<ServiceResponse<List<CategoryResponseDTO>>> GetAllCategoriesAsync()
         {
-            throw new NotImplementedException();
+            var allCategories = await db.Categories.ToListAsync();
+
+            var mappedCategories = mapper.Map<List<CategoryResponseDTO>>(allCategories);
+
+            return new ServiceResponse<List<CategoryResponseDTO>>
+            {
+                Message = "All Categories Fetched",
+                Success = true,
+                Data = mappedCategories
+            };
         }
 
-        public Task<ServiceResponse<CategoryResponseDTO>> GetCategoryByIdAsync(int id)
+        public async Task<ServiceResponse<CategoryResponseDTO>> GetCategoryByIdAsync(int categoryId)
         {
-            throw new NotImplementedException();
+            var categoryById = await db.Categories.FirstOrDefaultAsync(x=> x.Id == categoryId);
+            if (categoryById == null)
+            {
+                return new ServiceResponse<CategoryResponseDTO>()
+                {
+                    Message = "cant find the category with given Id",
+                    Success = false,
+                    Data = null
+                };
+            }
+            var mappedForResponse = mapper.Map<CategoryResponseDTO>(categoryById);
+            return new ServiceResponse<CategoryResponseDTO>
+            {
+                Message = "Category Fetched!",
+                Success = true,
+                Data = mappedForResponse
+            };
         }
 
-        public Task<ServiceResponse<CategoryResponseDTO>> UpdateCategoryAsync(int id, CreateCategoryDTO dto)
+        public async Task<ServiceResponse<CategoryResponseDTO>> UpdateCategoryAsync(int categoryId, CreateCategoryDTO dto)
         {
-            throw new NotImplementedException();
+            var categoryById = await db.Categories.FirstOrDefaultAsync(x => x.Id == categoryId);
+            if (categoryById == null)
+            {
+                return new ServiceResponse<CategoryResponseDTO>()
+                {
+                    Message = "cant find the category with given Id",
+                    Success = false,
+                    Data = null
+                };
+            }
+            categoryById.Name = dto.Name;
+            categoryById.Description = dto.Description;
+            await db.SaveChangesAsync();
+            var mappedIntoResponse = mapper.Map<CategoryResponseDTO>(categoryById);
+            return new ServiceResponse<CategoryResponseDTO>
+            {
+                Message = "Category Updated successfully!",
+                Success = true,
+                Data = mappedIntoResponse
+
+            };
         }
     }
 }
