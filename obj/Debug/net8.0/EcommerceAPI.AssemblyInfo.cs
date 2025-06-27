83c97631cb8e2754640ec2204ebcc644d4c722e8
Mutashim Mohsin <153811269+Mutashim99@users.noreply.github.com>
2025-06-28 01:15:31 +0500

    fix swagger ui for production

--- obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcommerceAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecd2706fe972e413e8c6fad370180611d987c32c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49fc73008536e28376eccb4bf87f7da1cca7352c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcommerceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
